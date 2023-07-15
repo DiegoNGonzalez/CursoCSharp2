76e06d7ada3b454ab844138a5d7cc5f5087897d2
Diego Gonzalez <diegonzalezv93@gmail.com>
2023-07-15 14:34:35 -0300

    Clases objetos propiedades

---
+++ Poo1/ejemplo1/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo1
+{
+    internal class Persona
+    {
+        //persona: Edad, Sueldo, Nombre
+        //atributos o miembros(donde va la información)
+        private int edad;
+        private float sueldo;
+        private string nombre;
+        //modificadores de visibilidad palabras reservadas
+        //publico(con acceso) privado(sin acceso)
+        
+        public void setEdad(int e)
+        {
+            edad = e;
+        }
+        public int getEdad()
+        {
+            return edad;
+        }
+    }
+}
