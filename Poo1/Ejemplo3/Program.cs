50d326bce0b06cd1bdb12427dcc27c4e9d1b7955
Diego Gonzalez <diegonzalezv93@gmail.com>
2023-07-16 18:03:04 -0300

    strings

---
+++ Poo1/Ejemplo3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+
+            nombre =Console.ReadLine();
+            nombre = "Hola cómo estás?????" + nombre;
+            //saber cantidad de caracteres
+            int cant = nombre.Length;
+            //poner todo en mayus
+            nombre= nombre.ToUpper();
+            //poner todo en minus
+            //nombre.ToLower();
+            //reemplazo de caracteres
+            nombre = nombre.Replace('A', 'E');
+            Console.WriteLine(nombre);
+            Console.ReadKey();
+        }
+    }
+}
