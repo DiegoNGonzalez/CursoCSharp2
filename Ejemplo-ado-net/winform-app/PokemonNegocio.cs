c3185c5ed6c40ecb270f4d58008bc6a28d04cf3d
Diego Nicolas Gonzalez Valenzuela <diegonzalezv93@gmail.com>
2023-11-18 17:06:06 -0300

    conexion a BD

---
+++ Ejemplo-ado-net/winform-app/PokemonNegocio.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace winform_app
+{
+    internal class PokemonNegocio
+    {
+        public List<Pokemon> Listar() {
+            List<Pokemon> list = new List<Pokemon>();
+            SqlConnection conexcion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+
+            try
+            {
+                conexcion.ConnectionString = "server=.\\SQLEXPRESS; database=POKEDEX_DB; integrated security=true; ";
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = "SELECT Numero,Nombre,Descripcion FROM POKEMONS;";
+                comando.Connection = conexcion;
+                conexcion.Open();
+                lector=comando.ExecuteReader();
+
+                while (lector.Read())
+                {
+                    Pokemon aux= new Pokemon();
+                    aux.Numero = lector.GetInt32(0);
+                    aux.Nombre = (string)lector["Nombre"];
+                    aux.Descripcion = (string)lector["Descripcion"];
+
+                    list.Add(aux); 
+                }
+               
+                return list;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally { conexcion.Close(); }
+
+
+        }
+    }
+}
